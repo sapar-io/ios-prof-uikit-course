8c6a3382bbb6348434d3d806f69fc6997e8eb95a
Sapar Jumabekov <sapa4tech@gmail.com>
2023-02-21 01:08:11 +0600

    Add login screen

---
+++ Bankey/Bankey/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Bankey
+//
+//  Created by Sapar Jumabekov on 20.02.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.backgroundColor = .systemBackground
+        window?.rootViewController = LoginViewController()
+        
+        return true
+    }
+    
+}
